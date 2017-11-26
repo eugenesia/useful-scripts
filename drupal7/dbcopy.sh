16c7b494b388a37283a0677068f4bf9868aa199d
Eugene Sia <eugene@eugenesia.co.uk>
2017-11-26 21:57:39 +0000

    Allow specifying db port

--- drupal7/dbcopy.sh
+++ drupal7/dbcopy.sh
@@ -8,16 +8,19 @@
 
 # Src database to copy from.
 srcHost='' # a
-srcDbUser='' # b
-srcDbPass='' # c
+srcPort='3306' # b
+srcDb='' # c
+srcDbUser='' # d
+srcDbPass='' # e
 
 # Dest database to copy to.
-destHost='' # d
-destDbUser='' # e
-destDbPass='' # f
+destHost='' # f
+destPort='3306' # g
+destDb='' # h
+destDbUser='' # i
+destDbPass='' # j
 
 # Name of database (same for both src and dest).
-dbName='' # g
 
 verbose=false # v
 
@@ -25,20 +28,24 @@ verbose=false # v
 delDestDb=false # x
 
 usageMsg="Copy Drupal 7 databases from one server to another.\n"\
-"Usage: $0 -a <srcHost> -b <srcDbUser> -c <srcDbPass> -d <destHost>\n"\
-"  -e <destDbUser> -f <destDbPass> -g <dbName> -v<verbose> -x<delDestDb>\n\n"\
-"Example: $0 -a sql.example1.com -b myuser -c MyPass\n"\
-"  -d sql.example2.com -e myawsuser -f MyAwsPass -g mydatabase -v -x"
+"Usage: $0 -a <srcHost> [-b <srcPort>] -c <srcDb> -d <srcDbUser> -e <srcDbPass>\n"\
+"  -f <destHost> [-g <destPort>] -h <destDb> -i <destDbUser> -j <destDbPass>\n"\
+"  [-v<verbose>] [-x<delDestDb>]\n\n"\
+"Example: $0 -a sql.example1.com -b 3306 -c src_db -d myuser -e MyPass\n"\
+"  -f sql.example2.com -g 3307 -h myawsuser -i MyAwsPass -j destdb -vx"
 
-while getopts 'a:b:c:d:e:f:g:vx' flag; do
+while getopts 'a:b:c:d:e:f:g:h:i:j:vx' flag; do
   case "${flag}" in
     a) srcHost=${OPTARG} ;;
-    b) srcDbUser=${OPTARG} ;;
-    c) srcDbPass=${OPTARG} ;;
-    d) destHost=${OPTARG} ;;
-    e) destDbUser=${OPTARG} ;;
-    f) destDbPass=${OPTARG} ;;
-    g) dbName=${OPTARG} ;;
+    b) srcPort=${OPTARG} ;;
+    c) srcDb=${OPTARG} ;;
+    d) srcDbUser=${OPTARG} ;;
+    e) srcDbPass=${OPTARG} ;;
+    f) destHost=${OPTARG} ;;
+    g) destPort=${OPTARG} ;;
+    h) destDb=${OPTARG} ;;
+    i) destDbUser=${OPTARG} ;;
+    j) destDbPass=${OPTARG} ;;
     v) verbose=true ;;
     x) delDestDb=false ;;
     *) echo -e "$usageMsg"
@@ -47,8 +54,8 @@ while getopts 'a:b:c:d:e:f:g:vx' flag; do
 done
 
 # Check for mandatory options.
-if [[ -z $srcHost || -z $srcDbUser || -z $srcDbPass || -z $destHost \
-  || -z $destDbUser || -z $destDbPass || -z $dbName ]]; then
+if [[ -z $srcHost || -z $srcDb || -z $srcDbUser || -z $srcDbPass \
+  || -z $destHost || -z $destDb || -z $destDbUser || -z $destDbPass ]]; then
   echo 'Error: Mandatory arguments not provided.'
   echo -e "$usageMsg"
   exit 1
@@ -131,8 +138,8 @@ getIgnoreParams() {
 # Main program
 
 # mysql/msqldump params.
-srcParams="-h $srcHost -u $srcDbUser -p$srcDbPass"
-destParams="-h $destHost -u $destDbUser -p$destDbPass"
+srcParams="-h $srcHost -P $srcPort -u $srcDbUser -p$srcDbPass"
+destParams="-h $destHost -P $destPort -u $destDbUser -p$destDbPass"
 
 # Turn on Bash verbosity.
 if [ "$verbose" = true ]; then
@@ -142,26 +149,26 @@ fi
 echo -e "Backing up \033[92m$dbName\033[0m"
 
 # Dump table structures
-mysqldump --no-data $srcParams $dbName > $dbName.sql
+mysqldump --no-data $srcParams $srcDb > $srcDb.sql
 
 # Dump table data, ignoring some tables.
-ignoreParams=$(getIgnoreParams $dbName)
-mysqldump $ignoreParams $srcParams $dbName >> $dbName.sql
+ignoreParams=$(getIgnoreParams $srcDb)
+mysqldump $ignoreParams $srcParams $srcDb >> $srcDb.sql
 
 echo -e "Migrating \033[92m$dbName\033[0m"
 
 if [ "$delDestDb" = true ]; then
-  mysql $destParams -e "DROP DATABASE IF EXISTS $dbName"
+  mysql $destParams -e "DROP DATABASE IF EXISTS $destDb"
 fi
 
-mysql $destParams -e "CREATE DATABASE IF NOT EXISTS $dbName"
+mysql $destParams -e "CREATE DATABASE IF NOT EXISTS $destDb"
 
 # Load data into dest table by "sourcing" the SQL commands. This seems to be
 # the proper way to restore Drupal databases.
-mysql $destParams $dbName < $dbName.sql
+mysql $destParams $destDb < $srcDb.sql
 
 # Delete temp SQL file.
-rm $dbName.sql
+rm $srcDb.sql
 
 # Turn off verbose.
 set +vx

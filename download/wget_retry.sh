b916e0caac585c872c9dc1b32437d5fbbdd82bef
Eugene Sia <eugene@eugenesia.co.uk>
2019-03-29 12:09:40 +0000

    Allow specifying output doc

--- download/wget_retry.sh
+++ download/wget_retry.sh
@@ -3,8 +3,20 @@
 # Retry wget until succeeded
 # https://superuser.com/questions/493640/how-to-retry-connections-with-wget
 
+url=$1
+output=$2
+
 while [ 1 ]; do
-  wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --continue $1
+  wget \
+    --retry-connrefused \
+    --waitretry=1 \
+    --read-timeout=20 \
+    --timeout=15 \
+    --tries=0 \
+    --continue \
+    --output-document=$output
+    -- $url
+
   if [ $? = 0 ]; then break; fi; # check return value, break if successful (0)
   sleep 1s;
 done;
